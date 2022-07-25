ea2cb1bcb1bb757eb0a03d3cb8956cb0326f83f6
Angelo Vicente <angelo.vicente@veolia.com>
2022-07-25 14:45:11 -0300

    [main] Create docker

---
+++ medical-discovery-server/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "==================================="
+echo "== BUILDING: course-mc-discovery =="
+echo "==================================="
+docker build --tag course-mc-discovery .
