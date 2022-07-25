631e16ce4ebe37975b47ae8c5ee45e7e41e1c633
Angelo Vicente <angelo.vicente@veolia.com>
2022-07-25 14:50:06 -0300

    [main] Config Server

---
+++ medical-config-server/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "======================================"
+echo "== BUILDING: course-mc-configserver =="
+echo "======================================"
+docker build --tag course-mc-configserver .
